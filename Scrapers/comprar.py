d0e50f4d2a9aecd0dee12c33f6a895af3a0cd97b
Fede <fede.collasius@gmail.com>
2023-05-19 01:54:18 -0300

    Stufff

--- Scrapers/comprar.py
+++ Scrapers/comprar.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.edge.options import Options
+from selenium.common.exceptions import TimeoutException
 
 import pandas as pd
 
@@ -39,16 +40,64 @@ driver.find_element(By.XPATH, '//*[@id="ctl00_CPH1_devDteEdtFechaAperturaHasta_I
 
 driver.find_element(By.XPATH, '//*[@id="ctl00_CPH1_btnListarPublicacionAvanzado"]').click()
 
-table = driver.find_element(By.XPATH, '//*[@id="ctl00_CPH1_GridListaPliegos"]')
-rows = table.find_elements(By.TAG_NAME, "tr") 
+try:
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="ctl00_CPH1_GridListaPliegos"]'))
+    )
+except TimeoutException:
+    print("Timed out waiting for page to load") 
 
-for row in rows:
-    cells = row.find_elements(By.TAG_NAME, "td")
-    if len(cells) > 0:
-        tender = {}
-        tender["title"] = cells[0].text
-        tender["entity"] = cells[1].text
-        tender["date"] = cells[2].text
-        tender["link"] = cells[3].find_element(By.TAG_NAME, "a").get_attribute("href")
-        tenders.append(tender)
+# Locate the table
+# Locate the table
+table = driver.find_element(By.ID, 'ctl00_CPH1_GridListaPliegos')
+
+# Get all the 'tr' elements in the table
+rows = table.find_elements(By.TAG_NAME, "tr")
+
+# Now 'data' is a list of lists, each inner list having the hyperlink from the first column and the text from the fourth column.
+tenders = []
+
+# Iterate over the rows
+for i, row in enumerate(rows):
+    # Get 'td' elements inside the 'tr'
+    cols = row.find_elements(By.TAG_NAME, "td")
+
+    # Ensure there are at least four columns
+    if len(cols) >= 4:
+        # First column with hyperlink
+        link = cols[0].find_element(By.TAG_NAME, "a").get_attribute('href')
+
+        # Save the current window handle
+        original_window = driver.current_window_handle
+
+        # Open a new tab
+        driver.execute_script("window.open();")
+
+        # Switch to the new tab
+        driver.switch_to.window(driver.window_handles[-1])
+
+        # Navigate to the URL in the new tab
+        driver.get(link)
+
+        # Fourth column with text
+        text = cols[3].find_element(By.TAG_NAME, "p").find_element(By.TAG_NAME, "span").text
+
+        # After you're done, close the new tab
+        driver.close()
+
+        # And switch back to the original tab
+        driver.switch_to.window(original_window)
+
+        # Append data
+        tenders.append([link, text])
+
+    else:
+        print(f"Skipping row {i} as it doesn't contain 4 columns")
+
+driver.quit()
+
+print(tenders)
+
+
+
 
--- Scrapers/test.py
+++ Scrapers/test.py
@@ -3,3 +3,19 @@ from datetime import datetime, timedelta
 list = ["hola"]
 print(len(list))
 
+# First column with hyperlink
+    hyperlink = cols[0].find_element(By.TAG_NAME, "a").get_attribute('href')
+
+    # Fourth column with text
+    # Ensure there are at least four columns
+    if len(cols) >= 4:
+        text = cols[3].text
+
+        # append data
+        data.append([hyperlink, text])
+    else:
+        print("Row didn't have 4 columns")
+
+
+
+
